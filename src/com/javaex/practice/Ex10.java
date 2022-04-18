4b29b5a1a0521513fcd76d18f370cc510fb9b325
최정필 <wjdvlf1407@gmail.com>
2022-04-18 23:53:03 +0900

    1.102 문제풀이

--- src/com/javaex/practice/Ex10.java
+++ src/com/javaex/practice/Ex10.java
@@ -3,54 +3,29 @@ package com.javaex.practice;
 import java.util.Scanner;
 
 public class Ex10 {
-	
+
 	public static void main(String[] args) {
-		
+
+		// 문제10번: 정수 다섯개를 입력받아 가장 큰 수를 출력하세요.
+
 		Scanner sc = new Scanner(System.in);
-		
+
+		int max=0;
 		System.out.println("숫자를 입력하세요");
-		
-		System.out.print("숫자: ");
-		int num1 = sc.nextInt();
-		
-		
-		System.out.print("숫자: ");
-		int num2 = sc.nextInt();
-		
-		
-		System.out.print("숫자: ");
-		int num3 = sc.nextInt();
-		
-		
-		System.out.print("숫자: ");
-		int num4 = sc.nextInt();
-		
-		
-		System.out.print("숫자: ");
-		int num5 = sc.nextInt();
-		
-		//	조건문
-		int max;
+
+		for (int i = 1; i <= 5; i++) {
+			System.out.print("숫자: ");
+			int num = sc.nextInt();
 			
-			if(num1>num2 && num1>num3 && num1>num4 && num1>num5) {
-				max=num1;
-			} else if(num2>num1 && num2>num3 && num2>num4 && num2>num5) {
-				max=num2;
-				
-			} else if(num3>num1 && num3>num2 && num3>num4 && num3>num5) {
-				max=num3;
-				
-			} else if(num4>num1 && num4>num2 && num4>num3 && num4>num5) {
-				max=num4;
-				
-			} else if(num5>num1 && num5>num2 && num5>num3 && num5>num4) {
-				max=num5;
-				
-		
-		System.out.println("최대값은 "+max+"입니다.");
-		
+			
+			if (num>=max) {
+				max = num;
+
+			}
+
 		}
-			sc.close();
+		System.out.println(max);
+		sc.close();
 	}
 
 }
