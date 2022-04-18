e842926d66c47434dfa03e514c34a3112377834d
최정필 <wjdvlf1407@gmail.com>
2022-04-18 17:52:36 +0900

    1.011 Ex01 출력 전 예상

--- src/com/javaex/practice/Ex01.java
+++ src/com/javaex/practice/Ex01.java
@@ -7,16 +7,35 @@ public class Ex01 {
 		
 		//문제1번: 아래의 출력결과를 에상하여 작성하세요
 		
-		System.out.println();
+		/*
+		예상
+		while문
+		index=0
+		index=1
+		index=2
+		
+		""사이에 들어가면 문자가 되기 때문에 변수명이 들어가도 변수값으로 나오지 않고 문자로 나온다.
+		*/
+
+		System.out.println("while문");
 		int index = 0;
 		while (index < 3) {
 			System.out.println("index=" + index);
 			index++;
 		}
 		
+		/*
+		예상
+		for문
+		order=0
+		order=1
+		order=2
+		
+		""사이에 들어가면 문자가 되기 때문에 변수명이 들어가도 변수값으로 나오지 않고 문자로 나온다.
+		*/
 		System.out.println("for문");
 		for (int order = 0; order < 3; order++) {
-			System.out.println("ordr=" + order);
+			System.out.println("order=" + order);
 			
 		}
 		
