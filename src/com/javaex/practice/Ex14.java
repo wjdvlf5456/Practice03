cc6daf38dff73ed978bf2f319ba69d8c27e410d6
최정필 <wjdvlf1407@gmail.com>
2022-04-18 22:17:13 +0900

    1.141 Ex14 출력전

---
+++ src/com/javaex/practice/Ex14.java
@@ -0,0 +1,30 @@
+package com.javaex.practice;
+
+import java.util.Scanner;
+
+public class Ex14 {
+	
+	public static void main(String[] args) {
+		
+		
+		//문제14번: 13번 문제에 계산식이 표시되도록 프로그램을 작성하세요.
+		
+		Scanner sc = new Scanner(System.in);
+		System.out.print("숫자를 입력하세요: ");
+		
+		int num = sc.nextInt();
+		int sum = 0;
+		
+		for (int i = 1; i <=num; i++) {
+			sum=sum+i;
+			System.out.print(i+"+");
+			
+		}
+		System.out.print("합계: "+sum);
+		
+		
+		sc.close();
+		
+	}
+
+}
