639b432a31c92456102934dbbe9ea3ead78997d5
최정필 <wjdvlf1407@gmail.com>
2022-04-19 03:04:19 +0900

    1.112 Ex11 주석정리

--- src/com/javaex/practice/Ex11.java
+++ src/com/javaex/practice/Ex11.java
@@ -3,38 +3,34 @@ package com.javaex.practice;
 import java.util.Scanner;
 
 public class Ex11 {
-	
+
 	public static void main(String[] args) {
-		
-		
-		//	문제풀이: %2와 while을 사용해준다.
-		
-		 
+
+		//문제11번: 입력받은 숫자가 홀수면 입력 값까지 홀수의 합을 출력합니다. 짝수면 짝수의 합; 
 		
 		Scanner sc = new Scanner(System.in);
-		
+
 		System.out.println("숫자를 입력하세요");
-		
+
 		int num = sc.nextInt();
 		int sum = 0;
 		
-		for (int i = 1; i<=num; i++) {
-			
-			if (i%2==0) {
-				sum=sum+i;
-				
-			} else {
-				sum=sum+i;
-				
-			} 
-			System.out.print("결과값: "+sum);
-			
+		//	짝수
+		if (num % 2 == 0) {
+			for (int i = 1; i <= num; i++) {
+				sum = sum + ++i;
+			}
+		//	홀수
+		} else if (num % 2 != 0) {
+			for (int i = 0; i <= num; i++) {
+				sum = sum + ++i;
+			}
+
 		}
-		
-		
-		
+		System.out.print("결과값: " + sum);
+
 		sc.close();
-		
+
 	}
 
 }
