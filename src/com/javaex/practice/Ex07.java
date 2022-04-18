db8f17f1faa78531f896394acd99ae6deb361c3b
최정필 <wjdvlf1407@gmail.com>
2022-04-18 21:57:00 +0900

    1.072 출력 후

--- src/com/javaex/practice/Ex07.java
+++ src/com/javaex/practice/Ex07.java
@@ -3,24 +3,25 @@ package com.javaex.practice;
 import java.util.Scanner;
 
 public class Ex07 {
-	
-	public static void main(String[] args) {
 		
-		//문제7번: 숫자를 입력받아 숫자만큼 가로세로에 입력한 수가 나오게 하시오
-		
-		Scanner sc = new Scanner(System.in);
-		System.out.println("숫자를 입력하세요");
-		int x = sc.nextInt();
-		
-		for (x=1; x<=x; x++) {
+		public static void main(String[] args) {
 			
-			for (int y=1; y<=x; y++) {
-				System.out.print(x);
+			//문제7번: 숫자를 입력받아 숫자만큼 가로세로에 입력한 수가 나오게 하시오
+			
+			Scanner sc = new Scanner(System.in);
+			
+			System.out.println("숫자를 입력하세요");
+			
+			int num = sc.nextInt();
+			
+			for (int i=1; i<=num; i++) {
+				for (int j = 1; j<i ; j++) {
+					System.out.print(i);
+				}
+				System.out.println(i);
 			}
-			System.out.println();
+			
+			sc.close();
 		}
-		sc.close();
-		
-	}
 
 }
