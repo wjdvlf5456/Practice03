199c8462619504b8ffe7393982cddd350a21de6d
최정필 <wjdvlf1407@gmail.com>
2022-04-18 19:16:06 +0900

    1.082 Ex08 출력 후

--- src/com/javaex/practice/Ex08.java
+++ src/com/javaex/practice/Ex08.java
@@ -9,9 +9,13 @@ public class Ex08 {
 		
 		
 		for (int i=1; i<=9; i++) {
+			
 			for (int j=2; j<=9; j++) {
-				System.out.print(j+"*"+i+"="+i*j);
+				
+				//출력되는 결과값
+				System.out.print(j+"*"+i+"="+i*j+"\t");
 			}
+			System.out.println(); 
 		}
 
 	}
