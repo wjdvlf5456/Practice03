9a5ab9c0e2c13d40bf8154b513655ddafacc5a6c
최정필 <wjdvlf1407@gmail.com>
2022-04-18 17:48:10 +0900

    Practice03 1.0

---
+++ src/com/javaex/practice/Ex01.java
@@ -0,0 +1,29 @@
+package com.javaex.practice;
+
+public class Ex01 {
+	
+	public static void main(String[] args) {
+		
+		
+		//문제1번: 아래의 출력결과를 에상하여 작성하세요
+		
+		System.out.println();
+		int index = 0;
+		while (index < 3) {
+			System.out.println("index=" + index);
+			index++;
+		}
+		
+		System.out.println("for문");
+		for (int order = 0; order < 3; order++) {
+			System.out.println("ordr=" + order);
+			
+		}
+		
+		
+		
+		
+		
+	}
+
+}
