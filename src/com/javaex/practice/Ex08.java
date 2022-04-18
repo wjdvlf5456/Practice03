f3253e8c7bc343bc4ef0c69b8c6986b0e91b7a1a
최정필 <wjdvlf1407@gmail.com>
2022-04-18 19:12:03 +0900

    1.081 Ex08 출력 전

---
+++ src/com/javaex/practice/Ex08.java
@@ -0,0 +1,19 @@
+package com.javaex.practice;
+
+public class Ex08 {
+
+	public static void main(String[] args) {
+		
+		
+		//문제8번: 구구단을 세로로 출력하세요
+		
+		
+		for (int i=1; i<=9; i++) {
+			for (int j=2; j<=9; j++) {
+				System.out.print(j+"*"+i+"="+i*j);
+			}
+		}
+
+	}
+
+}
