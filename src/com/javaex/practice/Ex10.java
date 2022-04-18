f48dc32cac84bf4a7f841575e6b9c00980d38606
최정필 <wjdvlf1407@gmail.com>
2022-04-19 01:33:40 +0900

    1.104 Ex10 다시해보기

--- src/com/javaex/practice/Ex10.java
+++ src/com/javaex/practice/Ex10.java
@@ -9,7 +9,21 @@ public class Ex10 {
 		// 문제10번: 정수 다섯개를 입력받아 가장 큰 수를 출력하세요.
 
 		Scanner sc = new Scanner(System.in);
-
+		System.out.println("숫자를 입력하세요");
+		
+		
+		int max=0;
+		
+		for (int i = 1; i <=5; i++) {
+			System.out.print("숫자: ");
+			int num = sc.nextInt();
+			
+			if (num>max) {
+				max=num;
+			}
+		}
+		System.out.println("최대값은 "+max+" 입니다.");
+		
 		sc.close();
 	}
 
