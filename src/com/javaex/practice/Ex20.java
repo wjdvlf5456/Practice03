cd28219a5e5d870c3eeb0b0a4004fb306cf873db
최정필 <wjdvlf1407@gmail.com>
2022-04-19 09:35:20 +0900

    1.201 Ex20 문제 수정전

---
+++ src/com/javaex/practice/Ex20.java
@@ -0,0 +1,45 @@
+package com.javaex.practice;
+
+import java.util.Scanner;
+
+public class Ex20 {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+		int odd = 64;
+		System.out.println("=================================");
+		System.out.println("\t[숫자맞추기게임 시작]");
+		System.out.println("=================================");
+
+		while (true) {
+			System.out.print(">>");
+			int num = sc.nextInt();
+
+			if (num < odd) {
+				System.out.println("더 높게");
+			} else if (num > odd) {
+				System.out.println("더 낮게");
+
+			} else if (num == odd) {
+				System.out.println("맞았습니다.");
+				System.out.print("게임을 종료하시겠습니까?(y/n)");
+				String end = sc.next();
+				
+				if (end == "n") {
+					System.out.println("계속합니다.");
+					continue;
+				} else if(end == "y") { 
+				}
+				System.out.println("=================================");
+				System.out.println("\t[숫자맞추기게임 종료]");
+				System.out.println("=================================");
+				break;
+			}
+
+		}
+
+		sc.close();
+	}
+
+}
