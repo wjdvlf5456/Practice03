4d2b590ca2863fc3c2c96773c9660c75e0877da5
최정필 <wjdvlf1407@gmail.com>
2022-04-19 15:58:00 +0900

    1.205 문제 equals 사용하여 수정

--- src/com/javaex/practice/Ex20.java
+++ src/com/javaex/practice/Ex20.java
@@ -20,12 +20,15 @@ public class Ex20 {
 				System.out.print("게임을 종료하시겠습니까?(y/n)");
 				String end = sc.next();
 
-					if (end == "y") {
+					if (end.equals("y")) {
 						System.out.println("=================================");
 						System.out.println("\t[숫자맞추기게임 종료]");
 						System.out.println("=================================");
 						break;
-					} else if (end == "n") {
+					} else if (end.equals("n")) {
+						System.out.println("=================================");
+						System.out.println("\t[숫자맞추기게임 시작]");
+						System.out.println("=================================");
 						continue;
 				}
 			} else if (num < odd) {
