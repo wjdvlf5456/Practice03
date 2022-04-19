184d525dbdbf139dd0b4ae3c7cc8c4893bec9274
최정필 <wjdvlf1407@gmail.com>
2022-04-19 11:26:27 +0900

    1.204 문제 수

--- src/com/javaex/practice/Ex20.java
+++ src/com/javaex/practice/Ex20.java
@@ -7,10 +7,10 @@ public class Ex20 {
 	public static void main(String[] args) {
 
 		Scanner sc = new Scanner(System.in);
-		int odd = 64;
 		System.out.println("=================================");
 		System.out.println("\t[숫자맞추기게임 시작]");
 		System.out.println("=================================");
+		int odd = 64;
 
 		while (true) {
 			System.out.print(">>");
@@ -19,22 +19,23 @@ public class Ex20 {
 				System.out.println("맞았습니다.");
 				System.out.print("게임을 종료하시겠습니까?(y/n)");
 				String end = sc.next();
-				if (end==("y")) {
-					System.out.println("=================================");
-					System.out.println("\t[숫자맞추기게임 종료]");
-					System.out.println("=================================");
-					break;
-				} else if(end==("n")) {
-					
-				} 
-				
+
+					if (end == "y") {
+						System.out.println("=================================");
+						System.out.println("\t[숫자맞추기게임 종료]");
+						System.out.println("=================================");
+						break;
+					} else if (end == "n") {
+						continue;
+				}
 			} else if (num < odd) {
 				System.out.println("더 높게");
 
 			} else if (num > odd) {
 				System.out.println("더 낮게");
-				
-			}
-		}sc.close();
 
-}}
+			}
+		}
+		sc.close();
+	}
+}
