fc44b207de03d6171e35e63a83c8e2f53707f24f
최정필 <wjdvlf1407@gmail.com>
2022-04-19 10:56:44 +0900

    1.203 문제 수정중

--- src/com/javaex/practice/Ex20.java
+++ src/com/javaex/practice/Ex20.java
@@ -15,33 +15,26 @@ public class Ex20 {
 		while (true) {
 			System.out.print(">>");
 			int num = sc.nextInt();
-			if (num < odd) {
-				System.out.println("더 높게");
-			} else if (num > odd) {
-				System.out.println("더 낮게");
-
-			} else if (num == odd) {
+			if (num == odd) {
 				System.out.println("맞았습니다.");
 				System.out.print("게임을 종료하시겠습니까?(y/n)");
-				continue;
+				String end = sc.next();
+				if (end==("y")) {
+					System.out.println("=================================");
+					System.out.println("\t[숫자맞추기게임 종료]");
+					System.out.println("=================================");
+					break;
+				} else if(end==("n")) {
+					
+				} 
 				
-			}	
-			sc.close();
-		}
+			} else if (num < odd) {
+				System.out.println("더 높게");
 
-	}
-}
-/*				while (true) {
+			} else if (num > odd) {
+				System.out.println("더 낮게");
+				
+			}
+		}sc.close();
 
-					if (alphabet == "n") {
-						end = alphabet;
-						System.out.println("=================================");
-						System.out.println("\t[숫자맞추기게임 시작]");
-						System.out.println("=================================");
-						continue;
-					} else if (alphabet == "y") {
-						end = alphabet;
-						System.out.println("=================================");
-						System.out.println("\t[숫자맞추기게임 종료]");
-						System.out.println("=================================");
-						break;}*/
+}}
